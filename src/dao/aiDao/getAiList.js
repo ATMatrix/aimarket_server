95fe6bbbb7b3b8d28ee2cdead76c2668d87476fc
HuJingwei <a479540195@gmail.com>
2018-01-26 11:40:36 +0800

    params

--- src/dao/aiDao/getAiList.js
+++ src/dao/aiDao/getAiList.js
@@ -16,7 +16,11 @@ export function getAiList(module, method, params) {
     //some code
     console.log('aiDao-getAiList');
     console.log(params) //todo::根据params 返回相应结果，加入ORM：sequelize
-    let SQL = `SELECT * FROM t_ai`
+    let SQL = `select * from \`t_ai\` ai
+    left join \`t_ai_api\` api on ai.\`AI_ID\` = api.\`AI_ID\`
+    left join \`t_ai_request\` req on req.\`AI_ID\` = ai.\`AI_ID\`
+    left join \`t_request_form\` form on req.\`REQUEST_ID\` = form.\`REQUEST_ID\`
+    left join \`t_form_field\` field on field.\`FORM_ID\` = form.\`FORM_ID\``    
     return new Promise((resolve, reject) => {
       poolConnection().then((conn)=>{
           conn.query(SQL, (error, results, fields) => {
@@ -24,7 +28,7 @@ export function getAiList(module, method, params) {
                   reject(error);
               }
               conn.release();
-              resolve(postInterceptor(results));
+              resolve(postInterceptor(results));            //todo::未考虑多个feild情况 需要解析feild
           });
       }).catch((error)=>{
           reject(error);
@@ -36,7 +40,12 @@ export function getAiList(module, method, params) {
 export function getAiInfo(module, method, id) {
     //some code
     console.log('aiDao-getAiList');
-    let SQL = `SELECT * FROM t_ai where t_ai.AI_ID = ${id}`
+    let SQL = `select * from \`t_ai\` ai
+    left join \`t_ai_api\` api on ai.\`AI_ID\` = api.\`AI_ID\`
+    left join \`t_ai_request\` req on req.\`AI_ID\` = ai.\`AI_ID\`
+    left join \`t_request_form\` form on req.\`REQUEST_ID\` = form.\`REQUEST_ID\`
+    left join \`t_form_field\` field on field.\`FORM_ID\` = form.\`FORM_ID\`
+    where ai.\`AI_ID\` = ${id}`
     return new Promise((resolve, reject) => {
       poolConnection().then((conn)=>{
           conn.query(SQL, (error, results, fields) => {
--- src/router/bill_router.js
+++ src/router/bill_router.js
@@ -27,8 +27,18 @@ billRouter.get('/api/1/getAiList', async (req, res) => {
       "type": o.AI_TECHNICAL_TYPE,
       "name": o.AI_NAME_EN_SHORT,
       "bill_type": o.AI_BILLING_TYPE,
+      "bill_kovan_addr": o.AI_BILLING_CONTRACT_KOVANNET_ADDR,
       "arg0": o.AI_ARG0,
-      "arg1": o.AI_ARG1
+      "arg1": o.AI_ARG1,
+      "dbot_url": o.AI_DBOT_URL,
+      "dbot_receiver": o.AI_DBOT_PROVIDER_ADDR,
+      "params": [{
+        "name": o.NAME,
+        "type": o.TYPE,
+        "required": o.REQUIRED,
+        "describe": o.DESCRIBE,
+        "example": o.PLACE_HOLDER
+      }]
     })
     result.push(aiInfo);
   }
@@ -39,13 +49,24 @@ billRouter.get('/api/1/getAiInfo', async (req, res) => {
   let objs = await getAiInfo(req.query.id);
   let result;
   for (let o of objs) {
+    console.log(o)
     let aiInfo = Object.assign({}, {
       "id": o.AI_ID,
       "type": o.AI_TECHNICAL_TYPE,
       "name": o.AI_NAME_EN_SHORT,
       "bill_type": o.AI_BILLING_TYPE,
+      "bill_kovan_addr": o.AI_BILLING_CONTRACT_KOVANNET_ADDR,
       "arg0": o.AI_ARG0,
-      "arg1": o.AI_ARG1
+      "arg1": o.AI_ARG1,
+      "dbot_url": o.AI_DBOT_URL,
+      "dbot_receiver": o.AI_DBOT_PROVIDER_ADDR,
+      "params": [{
+        "name": o.NAME,
+        "type": o.TYPE,
+        "required": o.REQUIRED,
+        "describe": o.DESCRIBE,
+        "example": o.PLACE_HOLDER
+      }]
     })
     result = aiInfo;
   }
